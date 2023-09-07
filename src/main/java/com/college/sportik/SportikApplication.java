552ee5f44621c4a87192b4c8d89c24a6f03fcdf4
egor.lyadsky <egor.lyadsky@sebbia.org>
2023-09-07 15:58:07 +0300

    initial commit

---
+++ src/main/java/com/college/sportik/SportikApplication.java
@@ -0,0 +1,13 @@
+package com.college.sportik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/college/sportik/SportikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.sportik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
